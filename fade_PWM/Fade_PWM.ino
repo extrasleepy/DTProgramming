b8a1bf8f165f9098239c6e9db89a4e482efca146
extrasleepy <akleindolph@gmail.com>
2015-01-03 15:52:08 -0800

    code simplified

--- fade_PWM/Fade_PWM.ino
+++ fade_PWM/Fade_PWM.ino
@@ -1,57 +1,44 @@
 // LED Fadeing Example (PWM)   http://arduino.cc/en/Reference/AnalogWrite
 
-int led0 = 5;           // the pin that the LED is attached to
-int led1 = 6;           // the pin that the LED is attached to
-int led2 = 7;           // the pin that the LED is attached to NO PWM
-int led3 = 8;           // the pin that the LED is attached to NO PWM
-int led4 = 9;           // the pin that the LED is attached to
-int led5 = 10;          // the pin that the LED is attached to
-int led6 = 11;          // the pin that the LED is attached to
-int led7 = 12;          // the pin that the LED is attached to NO PWM
-int brightness = 0;     // LED brightness
+byte brightness = 0;     // LED brightness
 
 void setup()  { 
-  pinMode(led0, OUTPUT);  
-  pinMode(led1, OUTPUT);  
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
-  pinMode(led5, OUTPUT);
-  pinMode(led6, OUTPUT);
-  pinMode(led7, OUTPUT);
+  pinMode(5, OUTPUT);  
+  pinMode(6, OUTPUT);  
+  pinMode(7, OUTPUT); //NO PWM
+  pinMode(8, OUTPUT); //NO PWM
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT); //NO PWM
 } 
 
 void loop()  { 
   //------fade in loop--------------
-  for (int fadein=0;fadein<254;fadein++){
+  for (int brightness=0;brightness<=254;brightness++){
     // set the brightness for the LEDs that support PWM
-    analogWrite(led0, brightness);
-    analogWrite(led1, brightness);
-    analogWrite(led4, brightness);
-    analogWrite(led5, brightness);  
-    analogWrite(led6, brightness);  
-
-    // change the brightness for next time through the loop
-    brightness = brightness + 1;
-    // wait for 2 milliseconds to see the dimming effect    
+    analogWrite(5, brightness);
+    analogWrite(6, brightness);
+    analogWrite(9, brightness);
+    analogWrite(10, brightness);  
+    analogWrite(11, brightness);  
+    // wait for 2 milliseconds to see the effect    
     delay(2); 
   }
   //-----end of fade in loop---------
+  
   //------fade out loop--------------
-  for (int fadeout=0;fadeout<254;fadeout++){
+   for (int brightness=254;brightness>=0;brightness--){
     // set the brightness for the LEDs that support PWM
-    analogWrite(led0, brightness);
-    analogWrite(led1, brightness);
-    analogWrite(led4, brightness);
-    analogWrite(led5, brightness);  
-    analogWrite(led6, brightness);  
-
-    // change the brightness for next time through the loop
-    brightness = brightness - 1;
-    // wait for 2 milliseconds to see the dimming effect    
+    analogWrite(5, brightness);
+    analogWrite(6, brightness);
+    analogWrite(9, brightness);
+    analogWrite(10, brightness);  
+    analogWrite(11, brightness);  
+    // wait for 2 milliseconds to see the effect    
     delay(2); 
   }
-  //-----end of fade in loop---------
+  //-----end of fade out loop---------
 
   brightness=0;   //make sure brightness is set at before loop restarts
 }
