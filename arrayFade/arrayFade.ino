1f18f3b54e91d2001d96856bd3757b39443a6678
extrasleepy <akleindolph@gmail.com>
2015-01-03 16:19:52 -0800

    revised

--- arrayFade/arrayFade.ino
+++ arrayFade/arrayFade.ino
@@ -1,25 +1,25 @@
 // Array Fade Example  http://arduino.cc/en/Reference/Array
 int myArray[] = { 
   5, 6, 9, 10, 11};               // an array of pin numbers to which LEDs are attached
-int totalPinsUsed = 5;           // the number of pins (i.e. the length of the array)
+int pins = 5;           // the number of pins (i.e. the length of the array)
 
 // the setup routine runs once when you press reset:
 void setup()  { 
-  for (int currentPin = 0; currentPin < totalPinsUsed; currentPin++)  {     //use an array to set all pins as outputs
-    pinMode(myArray[currentPin], OUTPUT);      
+  for (int i = 0; i < pins; i++)  {     //use an array to set all pins as outputs
+    pinMode(myArray[i], OUTPUT);      
   }
 } 
 
 void loop()  {                                                //fade LEDs in one at a time
-  for (int currentPin = 0; currentPin < totalPinsUsed; currentPin++) {      //do a fade for each pin
+  for (int i = 0; i < pins; i++) {      //do a fade for each pin
     for (int brightness=0;brightness<254;brightness++){       // set the brightness with this nested loop
-      analogWrite(myArray[currentPin],brightness);
+      analogWrite(myArray[i],brightness);
       delay(10);  
     }      
   }                                                          //fade LEDs out one at a time
-  for (int currentPin = totalPinsUsed; currentPin > 0; currentPin--) {
+  for (int i = pins; i > 0; i--) {
     for (int brightness=254;brightness>0;brightness--){
-      analogWrite(myArray[currentPin],brightness);
+      analogWrite(myArray[i],brightness);
       delay(10);  
     }                                                         //end brightness loop 
   }                                                           //end thisPin For loop
