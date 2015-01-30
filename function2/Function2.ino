d4dfcb4a241186dc34674ff769a40744f7f3e6dc
extrasleepy <akleindolph@gmail.com>
2015-01-30 11:03:14 -0800

    comments updated

--- function2/Function2.ino
+++ function2/Function2.ino
@@ -1,27 +1,27 @@
-
+// functions to turn all lights on and off
 void setup()  {           //set pins as outputs
   for (int counter=5; counter<13;counter++){
     pinMode (counter, OUTPUT);
   } 
 }
 
-void loop() {             //main loop
+void loop() {              //main loop
   allon();                 //call function
   delay (1000);
   alloff();                //call function
   delay (1000);
-}                         //end main loop
+}                          //end main loop
 
-void allon(){                                   //fade function starts here
+void allon(){                                   //function turns all lights on with for loop
   for (int counter=5; counter<13;counter++){
     digitalWrite (counter, HIGH);
   }  
-}//fade function ends here, return where main code left off
+}
 
-void alloff(){                                  //fade function starts here
+void alloff(){                                  //function turns all lights off with for loop
   for (int counter=5; counter<13;counter++){
     digitalWrite (counter, LOW);
   }   
-}//fade function ends here, return where main code left off
+}
 
 
