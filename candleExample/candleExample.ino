bbf32c8b2e81da997c070afafac8a4a1e4e17283
extrasleepy <akleindolph@gmail.com>
2015-01-30 14:36:34 -0800

    variable update

--- candleExample/candleExample.ino
+++ candleExample/candleExample.ino
@@ -2,20 +2,21 @@
  Make it look like a candle
  */
  float candle = 0.0;
+ byte led = 12;
 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+  pinMode(led, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   candle = random(0,20);
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(candle); 
   candle = random(0,20);  // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(candle);              // wait for a second
 }
