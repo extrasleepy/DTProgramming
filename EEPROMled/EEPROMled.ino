d41b95ce1b09f9ff5e444e8ad660cc132753c322
extrasleepy <akleindolph@gmail.com>
2015-02-25 15:32:11 -0800

    problem fixed

--- EEPROMled/EEPROMled.ino
+++ EEPROMled/EEPROMled.ino
@@ -7,11 +7,11 @@ int address = 0;                              //location in EEPROM
 void setup() {
   Serial.begin(9600);                         // sets up serial communication at 9600 bitss per second
   pinMode(led, OUTPUT);
- 
+
   stored = EEPROM.read(address);              // reads the data stored in address 0 and assigns to variable
   Serial.println(stored);                     // Serial print the value
   stored++;                                   //increment value
-  
+
   if (stored > 2) {                           //set this number based on how many different functions you want
     stored = 0;                              
   }
@@ -34,22 +34,27 @@ void loop() {                                 //one of 3 functions runs based on
 }
 
 void fast() {                                          
-  digitalWrite(led, !digitalRead(led));  //set light to opposite of current state
+  digitalWrite(led, HIGH);
   delay(100);
-  fast();
+  digitalWrite(led, LOW);
+  delay(100);
 }
 
 void slow() {
-  digitalWrite(led, !digitalRead(led));
+  digitalWrite(led, HIGH);
   delay(1000);
-  slow();
+  digitalWrite(led, LOW);
+  delay(1000);
 }
 
 void seizure() {
-  digitalWrite(led, !digitalRead(led));
-  delay(15);
-  seizure();
+  digitalWrite(led, HIGH);
+  delay(25);
+  digitalWrite(led, LOW);
+  delay(25);
+
 }
+
 
 
 
