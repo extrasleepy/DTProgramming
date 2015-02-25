94fa1f2ff6d4860f1dcd0de1f865daf267c66bfa
extrasleepy <akleindolph@gmail.com>
2015-02-25 15:19:11 -0800

    Variable updated

--- fadeOne_PWM/fadeOne_PWM.ino
+++ fadeOne_PWM/fadeOne_PWM.ino
@@ -7,11 +7,11 @@ void setup()  {
 
 void loop()  { 
   for(int i=0;i<256;i++){   //fade in
-    analogWrite(5, h);
+    analogWrite(5, i);
     delay(1);
   }
   for(int i=255;i>0;i--){   //fade in
-    analogWrite(5, h);
+    analogWrite(5, i);
     delay(1);
   }
 
