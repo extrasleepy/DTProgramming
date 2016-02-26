075937148a964b0719c833feb06ad88eef33c80b
Andrew Kleindolph <akleindolph@gmail.com>
2016-02-26 11:24:59 -0800

    improvements (including some by Natalie Freed : ))

--- arrayFade/arrayFade.ino
+++ arrayFade/arrayFade.ino
@@ -1,28 +1,26 @@
 // Array Fade Example  http://arduino.cc/en/Reference/Array
-int myArray[] = { 
-  5, 6, 9, 10, 11};               // an array of pin numbers to which LEDs are attached
-int pins = 5;           // the number of pins (i.e. the length of the array)
+int myArray[] = { 5, 6, 9, 10, 11};     // an array of pin numbers to which LEDs are attached (indices 0-4)
+int numPins = 5;                        // the number of pins in the array
 
-// the setup routine runs once when you press reset:
 void setup()  { 
-  for (int i = 0; i < pins; i++)  {     //use an array to set all pins as outputs
+  for (int i = 0; i < numPins; i++)  {                       //use an array to set all pins as outputs
     pinMode(myArray[i], OUTPUT);      
   }
 } 
 
-void loop()  {                                                //fade LEDs in one at a time
-  for (int i = 0; i < pins; i++) {      //do a fade for each pin
-    for (int brightness=0;brightness<254;brightness++){       // set the brightness with this nested loop
+void loop()  {                                               //fade LEDs in one at a time
+  for (int i = 0; i < numPins; i++) {                        //fade for each pin starting with array value 0, end with 4
+    for (int brightness=0;brightness<254;brightness++){      //set the brightness with this nested loop
       analogWrite(myArray[i],brightness);
       delay(10);  
     }      
   }                                                          //fade LEDs out one at a time
-  for (int i = pins; i > 0; i--) {
+  for (int i = numPins-1; i > 0; i--) {                      //start with array value 4, end with 0
     for (int brightness=254;brightness>0;brightness--){
       analogWrite(myArray[i],brightness);
       delay(10);  
-    }                                                         //end brightness loop 
-  }                                                           //end thisPin For loop
+    }                                                         //end brightness for loop 
+  }                                                           //end numPins for loop
 }                                                             //end main loop
 
 
