dae82b4b1ee7b929cba27a8f69c1181688e08601
Andrew Kleindolph <akleindolph@gmail.com>
2019-09-24 08:55:52 -0700

    Update esp8266_basicLEDtest.ino

--- esp8266_basicLEDtest/esp8266_basicLEDtest.ino
+++ esp8266_basicLEDtest/esp8266_basicLEDtest.ino
@@ -4,14 +4,14 @@
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "b875f63dc86944fa9b6bc0f404a3e6aa";
+char auth[] = "";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "ATT8WPP966";
-char pass[] = "9a99f38=4w=7";
-//char ssid[] = "StudentNet";
-//char pass[] = "4153334021";
+//char ssid[] = "";
+//char pass[] = "";
+char ssid[] = "StudentNet";
+char pass[] = "4153334021";
 
 #define ESP8266_BAUD 115200
 ESP8266 wifi(&Serial);
