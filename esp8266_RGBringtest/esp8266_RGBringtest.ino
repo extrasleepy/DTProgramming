643a9519135bbf13fef60a36840fd5675a3bef4e
Andrew Kleindolph <akleindolph@gmail.com>
2019-09-24 08:49:26 -0700

    ESP8266_examples

---
+++ esp8266_RGBringtest/esp8266_RGBringtest.ino
@@ -0,0 +1,75 @@
+#include <Adafruit_NeoPixel.h>
+#define BLYNK_PRINT Serial
+//#include <ESP8266_Lib.h>
+#include <BlynkSimpleShieldEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "b875f63dc86944fa9b6bc0f404a3e6aa";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "ATT8WPP966";
+char pass[] = "9a99f38=4w=7";
+
+#define ESP8266_BAUD 115200
+ESP8266 wifi(&Serial);
+
+#define PIN 6
+#define PIXEL_COUNT 12
+
+unsigned int dimmer = 254;
+unsigned int red = 254;
+unsigned int green = 254;
+unsigned int blue = 254;
+
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIN, NEO_GRB + NEO_KHZ800);  //use me
+//Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIN, NEO_GRBW + NEO_KHZ800);  //temp light
+
+void setup()
+{
+  // Debug console
+  Serial.begin(ESP8266_BAUD);
+  delay(10);
+
+Blynk.begin(auth, wifi, ssid, pass);
+
+  strip.begin();
+  strip.show();
+
+  for (int i = 0; i < strip.numPixels(); i++)
+  {
+    strip.setPixelColor(i, 255, 180, 0);  //default color
+    strip.show();
+    delay (100);
+  }
+}
+void loop()
+{
+  Blynk.run();
+}
+
+BLYNK_WRITE(V5)
+{
+  dimmer = param.asInt();
+
+  for (int i = 0; i < strip.numPixels(); i++)
+  {
+    strip.setPixelColor(i, (dimmer * red) / 255, (dimmer * green) / 255, (dimmer * blue) / 255);
+  }
+  strip.show();
+}
+
+BLYNK_WRITE(V6)
+{
+  red = param[0].asInt();
+  green = param[1].asInt();
+  blue = param[2].asInt();
+
+  for (int i = 0; i < strip.numPixels(); i++)
+  {
+    strip.setPixelColor(i, (dimmer * red) / 255, (dimmer * green) / 255, (dimmer * blue) / 255);
+  }
+  strip.show();
+}
